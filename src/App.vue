646e924b4469a4adcad49632888ef3251eccc7bf
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2021-04-09 22:30:43 -0300

    created app navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <AppHeader />
     <h1>Welcome to Vue.js App</h1>
   </div>
 </template>
 
 <script>
-
+import AppHeader from './components/AppHeader.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppHeader: AppHeader
+  }
 }
 </script>
 
