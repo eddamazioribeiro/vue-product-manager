629b1ae6fa003b593161acd5937a6122cf325d47
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2021-04-09 23:02:10 -0300

    add product component: added form

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -2,9 +2,29 @@
   <b-col md="4" class="m-3">
     <div>
       <b-card header="Add Product">
-        <b-card-text>
-        </b-card-text>
-        <b-button href="#" variant="primary">Add Product</b-button>
+        <b-card-body>
+          <b-form autocomplete="off">
+            <b-form-group
+              label="Name">
+              <b-form-input v-model="form.name" id="name" name="name" trim></b-form-input>
+            </b-form-group>
+            <b-form-group
+              label="Price ($)">
+              <b-form-input v-model="form.price" id="price" name="price" trim></b-form-input>
+            </b-form-group>
+            <b-form-group
+              label="Brand">
+              <b-form-input v-model="form.brand" id="brand" name="brand" trim></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              label="Inventory?">
+              <b-form-radio v-model="form.inventoryStatus" name="inventory-status" value="true">In stock</b-form-radio>
+              <b-form-radio v-model="form.inventoryStatus" name="inventory-status" value="false">Out of stock</b-form-radio>                                   
+            </b-form-group>
+          </b-form>
+        </b-card-body>
+        <b-button block variant="primary" @click="addProduct">Add Product</b-button>
       </b-card>
     </div>
   </b-col>
@@ -12,7 +32,21 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      form: {
+        name: '',
+        price: '',
+        brand: '',
+        inventoryStatus: ''
+      }
+    }
+  },
+  methods: {
+    addProduct() {
+      console.log(this.form)
+    }
+  }
 }
 </script>
 
